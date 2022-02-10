c69e924f1bac42a914a396f41474d6b993e3611e
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2022-02-10 18:21:04 +0100

    Give blue dot some personal space

--- src/components/Header/Nav/CartButton/index.tsx
+++ src/components/Header/Nav/CartButton/index.tsx
@@ -13,7 +13,7 @@ export function CartButton(props: Props) {
           <BiCart />
 
           {props.isNotEmpty && (
-            <span className="bg-blue-500 w-2 h-2 absolute -mt-4 -mr-6 rounded-full" />
+            <span className="bg-blue-500 w-3 h-3 absolute -mt-3 -mr-5 rounded-full border-3 border-white" />
           )}
         </div>
       </a>
