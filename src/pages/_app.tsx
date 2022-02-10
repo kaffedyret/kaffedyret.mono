f2c1868d64aead26f2a5fb75bca2aae6e6fb2bcb
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2022-02-10 22:23:24 +0100

    Show proper cart state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
-import "../styles/styles.css";
 import type { AppProps } from "next/app";
-import { Header } from "~/components/Header";
-import { Footer } from "~/components/Footer";
 import Head from "next/head";
+import nookies from "nookies";
+import { Footer } from "~/components/Footer";
+import { Header } from "~/components/Header";
+import { isCartNotEmpty } from "~/lib/cart";
+import { cartConfig } from "~/lib/cookies";
+import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const getCartState = (): boolean => {
+    const { cart } = nookies.get(null, cartConfig);
+    return !!cart && isCartNotEmpty(JSON.parse(cart));
+  };
+
   return (
     <>
       <Head>
@@ -13,7 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
 
       <div className="grid grid-rows-app min-h-screen">
-        <Header isCartNotEmpty={true} />
+        <Header isCartNotEmpty={getCartState()} />
 
         <section className="bg-slate-100">
           <Component {...pageProps} />
