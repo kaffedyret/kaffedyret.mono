e2b44de2dff30f97c86dda0973fb0150d463ecdd
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2022-02-10 13:50:40 +0100

    Add some global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/styles.css'
-import type { AppProps } from 'next/app'
+import "../styles/styles.css";
+import type { AppProps } from "next/app";
+import { Header } from "~/components/Header";
+import { Footer } from "~/components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-rows-app min-h-screen">
+      <Header isCartNotEmpty={true} />
+
+      <section className="bg-slate-100">
+        <Component {...pageProps} />
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
