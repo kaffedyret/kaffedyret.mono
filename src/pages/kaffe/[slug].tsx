69fc786441216d36eee1b4d8468d35d0bac6bd45
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2022-02-11 21:35:13 +0100

    Add another log

--- src/pages/kaffe/[slug].tsx
+++ src/pages/kaffe/[slug].tsx
@@ -161,12 +161,14 @@ export const getStaticProps = async ({
 }: GetStaticPropsContext<PageParams>): Promise<GetStaticPropsResult<Props>> => {
   const { slug } = params!;
 
+  console.log("/kaffe/[slug] slug:", slug);
+
   const [product] = await sanityClient.fetch<Product[]>(
     `*[_type == "product" && slug.current == $slug] | order(order asc) { _id, title, body, slug, available, defaultProductVariant, variants, blurb }`,
     { slug }
   );
 
-  console.log("/kaffe/[slug]", product);
+  console.log("/kaffe/[slug] product:", product);
 
   const allVariants = getAllVariantsFromProduct(product);
 
