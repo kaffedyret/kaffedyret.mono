a417091e523b8a615098847f45b0592ba71690d1
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2022-07-14 13:12:27 +0200

    Fix a bug where initial active tab was null

--- src/features/admin/hooks/useOrderStatusTabs.ts
+++ src/features/admin/hooks/useOrderStatusTabs.ts
@@ -3,7 +3,7 @@ import { OrderStatus } from "~/models/schema.sanity";
 
 export const useOrderStatusTabs = (orderStatuses?: OrderStatus[]) => {
   const [activeTabId, setActiveTabId] = useState<string | null>(
-    orderStatuses ? orderStatuses[0].name : null
+    orderStatuses ? orderStatuses[0]._id : null
   );
 
   const handleTabClick = (id: string) => {
